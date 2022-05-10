ee0acbc1ca7ca4c0352fe07c21c5c97fcb54784b
urioh <uriohjoe7@gmail.com>
2022-05-10 17:23:57 +0300

    first commit

---
+++ Hello.java
@@ -0,0 +1,14 @@
+public class Hello {
+
+    public static void main(String args[]){
+        double a=-9.81,v=0,x;
+         Integer t=0;
+
+        x = 0.5 * (a*t*t) + (v*t) + x
+
+
+                System.out.println(x);
+
+    }
+
+}
